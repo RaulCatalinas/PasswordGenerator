cedced64d1097a9474655bd413396746b495d69c
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2025-04-11 14:47:48 +0200

    chore: chore: start migrating the app from Python to Go
    - Initiated the migration process to Go for better performance

---
+++ internal/app_settings/window.go
@@ -0,0 +1,8 @@
+package app_settings
+
+const (
+	TITLE          = "PasswordGenerator"
+	WIDTH          = 1200
+	HEIGHT         = 400
+	DISABLE_RESIZE = true
+)
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+
+	appSettings "github.com/RaulCatalinas/PasswordGenerator/internal/app_settings"
+)
+
+//go:embed all:frontend/dist
+var assets embed.FS
+
+//go:embed build/appicon.png
+var icon []byte
+
+func main() {
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:            appSettings.TITLE,
+		Width:            appSettings.WIDTH,
+		Height:           appSettings.HEIGHT,
+		DisableResize:    appSettings.DISABLE_RESIZE,
+		Assets:           assets,
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+}
