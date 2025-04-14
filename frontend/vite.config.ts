78395d6812d6ab19a07019fe6346b0cfee03c1fb
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2025-04-14 23:47:21 +0200

    chore: Updated Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,12 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import type { UserConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()]
-})
+export default {
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  build: {
+    target: 'esnext'
+  }
+} satisfies UserConfig
