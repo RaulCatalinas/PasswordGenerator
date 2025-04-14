e7d56569f7a8f799e7090cebde1b356fe522d92d
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2025-04-14 13:09:03 +0200

    refactor: remove use-debounce since password is only generated on button click

--- frontend/src/components/sliders/Slider.tsx
+++ frontend/src/components/sliders/Slider.tsx
@@ -1,27 +1,13 @@
-// Store
 import { useDarkModeStore } from "@/src/stores/dark-mode"
 import { usePasswordStore } from "@/src/stores/password"
-
-// Third-Party libraries
-import { useDebounce } from "use-debounce"
-
-// React
-import { useEffect, useState } from "react"
 
 export default function Slider() {
   const darkModeActive = useDarkModeStore(state => state.darkModeActive)
   const setPasswordLength = usePasswordStore(state => state.setPasswordLength)
-
-  const [localPasswordLength, setLocalPasswordLength] = useState(8)
-
-  const [debouncedLength] = useDebounce(localPasswordLength, 500)
-
-  useEffect(() => {
-    setPasswordLength(debouncedLength)
-  }, [debouncedLength, setPasswordLength])
+  const passwordLength = usePasswordStore(state => state.passwordLength)
 
   const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
-    setLocalPasswordLength(parseInt(target.value))
+    setPasswordLength(parseInt(target.value))
   }
 
   return (
@@ -47,7 +33,7 @@ export default function Slider() {
             ${darkModeActive ? "text-gray-200" : "text-gray-800"}
           `}
         >
-          {localPasswordLength}
+          {passwordLength}
         </span>
       </div>
       <div
