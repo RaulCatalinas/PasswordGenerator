453aa35a67e7bd097b51762c73b48cd387f4e4ef
Raul Catalinas <raulcatalinas@gmail.com>
2025-11-22 13:21:26 +0100

    chore: update sacial_madia.dart file

--- lib/handlers/social_media.dart
+++ lib/handlers/social_media.dart
@@ -1,11 +1,12 @@
+import 'package:logkeeper/logkeeper.dart' show LogKeeper;
 import 'package:url_launcher/url_launcher.dart' show launchUrl;
 
 Future<void> openUrl(String url) async {
   if (!await launchUrl(Uri.parse(url))) {
-    print('The URL could not be opened: $url');
+    LogKeeper.error('The URL could not be opened: $url');
 
     return;
   }
 
-  print('URL $url opened correctly');
+  LogKeeper.info('URL $url opened correctly');
 }
